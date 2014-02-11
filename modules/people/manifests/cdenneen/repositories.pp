21938c7eedbf511973d8eb1e7837d2905ded8c07
Chris Denneen <cdenneen@gmail.com>
2014-02-11 17:07:56 -0500

    added puppet, gitolite, and interact-devops

--- modules/people/manifests/cdenneen/repositories.pp
+++ modules/people/manifests/cdenneen/repositories.pp
@@ -27,4 +27,13 @@ class people::cdenneen::repositories (
   repository { "${my_sourcedir}/dnp/apress":
     source => 'apress@svn-777.prod.hosting.acquia.com:apress.git'
   }
+  repository { "${my_sourcedir}/deploy":
+    source => 'git@gitolite:deploy.git'
+  }
+  repository { "${my_sourcedir}/gitolite-admin":
+    source => 'git@gitolite:gitolite-admin'
+  }
+  repository { "${my_sourcedir}/interact":
+    source => 'git@ctcinteract-svn01.ap.org:devops.git'
+  }
 }
